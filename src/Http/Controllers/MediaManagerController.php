52b7e56008c0105346890b16498ebe380ade7f0f
Massimo Triassi <massimo@plankdesign.com>
2020-06-11 16:32:53 -0400

    add create and destroy directory functionality

--- src/Http/Controllers/MediaManagerController.php
+++ src/Http/Controllers/MediaManagerController.php
@@ -1,26 +1,48 @@
 <?php
 namespace Plank\MediaManager\Http\Controllers;
 
+use App\Exceptions\MediaManagerException;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Storage;
+use Plank\Mediable\Media;
+use Plank\MediaManager\MediaManager;
 
 class MediaManagerController extends BaseController
 {
+    protected $manager;
+
+    public function __construct(MediaManager $manager)
+    {
+        $this->manager = $manager;
+    }
 
     public function index()
     {
-        // summon the media manager view?
+        return view('media-manager');
     }
 
     public function create()
     {
-        // create directory
+        $disk = $this->manager->verifyDisk($this->request->get('disk'));
+        $path = $this->manager->verifyDirectory($disk, $this->request->path);
+
+        if (Storage::disk($disk)->has($path)) {
+            throw MediaManagerException::directoryAlreadyExists($disk, $path);
+        }
+        Storage::disk($disk)->makeDirectory($path);
+
+        return response();
     }
 
     public function destroy()
     {
-        // delete a directory (and all the contained files)
+        $disk = $this->manager->verifyDisk($this->request->get('disk'));
+        $path = $this->manager->verifyDirectory($disk, $this->path);
+        Storage::disk($disk)->deleteDirectory($path);
+        Media::where('directory', $path)->delete();
+        return response();
     }
 }
