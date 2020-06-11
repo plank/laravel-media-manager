e25ef61f6d572e8d8d53b8f862919333cb0cf7a5
Massimo Triassi <massimo@plankdesign.com>
2020-06-11 11:43:05 -0400

    add basic routes file

--- src/Http/Controllers/MediaManagerController.php
+++ src/Http/Controllers/MediaManagerController.php
@@ -6,6 +6,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class MediaManagerController extends BaseController {
+class MediaManagerController extends BaseController
+{
 
 }
