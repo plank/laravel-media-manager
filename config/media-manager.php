950bc479d2239879f0ed6174605dcad25dd3cca1
Massimo Triassi <massimo@plankdesign.com>
2020-10-22 17:08:48 -0400

    #7 add comment about formatting to config file

--- config/media-manager.php
+++ config/media-manager.php
@@ -5,6 +5,7 @@
  */
 return [
     'model' => \Plank\MediaManager\Models\Media::class,
+    // The relative path to the conversions directory, **including** the trailing slash
     'conversions-directory' => "conversions/",
     'conversions' => [
         // 240 is the maximum image width, ie: 240w
