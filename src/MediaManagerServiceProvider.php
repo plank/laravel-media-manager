c0fbbb202d91889852dce98d13ce160df4b28d86
Massimo Triassi <massimo@plankdesign.com>
2021-03-02 14:28:52 -0500

    #1 add load migrations for package

--- src/MediaManagerServiceProvider.php
+++ src/MediaManagerServiceProvider.php
@@ -20,7 +20,7 @@ class MediaManagerServiceProvider extends ServiceProvider
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'media-manager');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'media-manager');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
          $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
         if ($this->app->runningInConsole()) {
