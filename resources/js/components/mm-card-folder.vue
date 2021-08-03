8cd5a8add7d22bd09513f30ec9c27c94b3e92c59
Massimo Triassi <massimo@plankdesign.com>
2021-08-03 14:08:52 -0400

    fix: #39 correct wording on i18n
    last modified on rather than created on

--- resources/js/components/mm-card-folder.vue
+++ resources/js/components/mm-card-folder.vue
@@ -4,7 +4,7 @@
       <svg width="20" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><g fill-rule="nonzero"><path d="M18.684 15.168H1.053A1.053 1.053 0 010 14.116V1.069C0 .488.471.017 1.053.017H6.03c.38 0 .73.205.917.536l.58 1.032c.187.332.538.537.918.537h10.239c.581 0 1.053.471 1.053 1.052v10.942c0 .581-.472 1.052-1.053 1.052z" fill="#AAA"/><path d="M1.053 15.505h17.631c.581 0 1.053-.47 1.053-1.052V3.512c0-.582-.472-1.053-1.053-1.053h-7.609c-.38 0-.73.205-.917.537l-.58 1.032a1.053 1.053 0 01-.918.536H1.053C.47 4.564 0 5.035 0 5.617v8.836c0 .581.471 1.052 1.053 1.052z" fill="gray"/></g></g></svg>
       <h6>{{ item.name | clearname }}</h6>
       <span class="date"
-        >{{ $t("actions.created_on") }}
+        >{{ $t("actions.modified_on") }}
         {{ item.timestamp | moment("MMMM Do, YYYY") }}</span
       >
     </div>
