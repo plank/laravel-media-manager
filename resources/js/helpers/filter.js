d83ec624d95b26bbaa8d328f014f596412e62aa4
jason camern <jason.cameron@plankdesign.com>
2021-08-09 16:33:53 -0400

    feat: #45 Lable for media upload file size

---
+++ resources/js/helpers/filter.js
@@ -0,0 +1,8 @@
+const fileSizeFilter = fileSize => {
+    let sizesPrefix = ["Bytes", "KB", "MB", "GB", "TB"];
+    if (fileSize == 0) return "0 Byte";
+    let i = parseInt(Math.floor(Math.log(fileSize) / Math.log(1024)));
+    return Math.round(fileSize / Math.pow(1024, i), 2) + " " + sizesPrefix[i];
+};
+
+export default fileSizeFilter;
