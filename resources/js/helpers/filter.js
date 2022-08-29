60fd27e5b89026819a80d68a4222171d2ceec912
Riley McMaster <rileymcmaster@gmail.com>
2022-08-29 13:31:37 -0400

    #78-1 - handle change lang bug

--- resources/js/helpers/filter.js
+++ resources/js/helpers/filter.js
@@ -1,8 +1,8 @@
-const fileSizeFilter = fileSize => {
-    let sizesPrefix = ["Bytes", "KB", "MB", "GB", "TB"];
-    if (fileSize == 0) return "0 Byte";
-    let i = parseInt(Math.floor(Math.log(fileSize) / Math.log(1024)));
-    return Math.round(fileSize / Math.pow(1024, i), 2) + " " + sizesPrefix[i];
-};
+const fileSizeFilter = (fileSize = 0) => {
+	let sizesPrefix = ["Bytes", "KB", "MB", "GB", "TB"]
+	if (fileSize == 0) return "0 Byte"
+	let i = parseInt(Math.floor(Math.log(fileSize) / Math.log(1024)))
+	return Math.round(fileSize / Math.pow(1024, i), 2) + " " + sizesPrefix[i]
+}
 
-export default fileSizeFilter;
+export default fileSizeFilter
