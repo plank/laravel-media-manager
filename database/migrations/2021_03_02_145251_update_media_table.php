a1aab74219a99a81f58345d802502af889f46ddb
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2025-06-24 14:21:56 -0500

    chore!: remove alt column from migration

--- database/migrations/2021_03_02_145251_update_media_table.php
+++ database/migrations/2021_03_02_145251_update_media_table.php
@@ -1,36 +1,35 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-class UpdateMediaTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class UpdateMediaTable extends Migration
     {
-        $adding = ['title', 'caption', 'alt', 'credit'];
-        if (!Schema::hasColumns(config('media-manager.table'), $adding)) {
-            Schema::table('media', function (Blueprint $table) {
-                $table->string('credit')->nullable()->after('id');
-                $table->string('alt')->nullable()->after('id');
-                $table->text('caption')->nullable()->after('id');
-                $table->string('title')->nullable()->after('id');
-            });
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            $adding = ['title', 'caption', 'alt', 'credit'];
+            if (!Schema::hasColumns(config('media-manager.table'), $adding)) {
+                Schema::table('media', function (Blueprint $table) {
+                    $table->string('credit')->nullable()->after('id');
+                    $table->text('caption')->nullable()->after('id');
+                    $table->string('title')->nullable()->after('id');
+                });
+            }
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            // Down will be handled when tables are removed.
         }
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        // Down will be handled when tables are removed.
-    }
-}
