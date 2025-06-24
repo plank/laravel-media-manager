1e23d7e902eda4a2d0ad122b1495efed67992302
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2025-06-24 14:24:53 -0500

    chore: addexcpetion handling to test

--- tests/Feature/DirectoryTest.php
+++ tests/Feature/DirectoryTest.php
@@ -136,7 +136,7 @@ class DirectoryTest extends TestCase
         $this->testSetup($initialState);
 
         // call endpoint
-        $response = $this->post(
+        $response = $this->withoutExceptionHandling()->post(
             route('media-api.directory.create'),
             [
                 'disk' => $this->disk,
